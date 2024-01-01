bb3e4e63bf382f39afe4d19590de8b87b326b18a
Alec Saunders <alec.saunders@iviful.com>
2024-01-01 12:55:45 -0500

    Show all shift buttons in line with each other.

--- CaptionsEditor/SubViews/Cue Views/TimeShiftView.swift
+++ CaptionsEditor/SubViews/Cue Views/TimeShiftView.swift
@@ -16,8 +16,23 @@ struct TimeShiftView: View {
     var isPositive: Bool {
         Int(shiftValue * 10.0) >= 0
     }
-    var shiftSymbol: String {
-        isPositive ? ">>" : "<<"
+    
+    var buttonPadding: CGFloat = 4.0
+    
+    var shiftAllSymbol: String {
+        "circle.grid.2x2.fill"
+    }
+    
+    var shiftOne: String {
+        isPositive ? "circle.grid.2x1.right.filled" : "circle.grid.2x1.left.filled"
+    }
+    
+    var shiftBoth: String {
+        "circle.grid.2x1.fill"
+    }
+    
+    var endAndStart: String {
+        "arrow.up.left.and.arrow.down.right"
     }
     
     /// The undo manager that the environment stores.
@@ -60,41 +75,103 @@ struct TimeShiftView: View {
                 }
                 .buttonStyle(.borderless)
                 Spacer()
-                Button(shiftSymbol) {
-                    document.shiftTimeValues(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
-                    showPopover = false
-                }
-                    .disabled(Int(shiftValue * 1000) == 0)
-                    .contextMenu {
-                        Section {
-                            Text("Shift \(isPositive ? "forward" : "backward")")
-                            Button("\(start ? "start" : "end") and remaining…") {
-                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
-                                showPopover = false
-                            }
-                        }
-                        
-                        Section {
-                            Button("\(start ? "start" : "end") only") {
-                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
-                                showPopover = false
-                            }
-                            Button("both") {
-                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: nil, undoManager: undoManager)
-                                showPopover = false
-                            }
-                            if !start {
-                                Button("end and start of next") {
-                                    document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: false, undoManager: undoManager)
-                                    let cueIndex = document.captions.getIndex(forCueID: cue.id)
-                                    let nextIndex = cueIndex + 1
-                                    let nextCue = document.captions.cues[nextIndex]
-                                    document.shiftTime(withValue: shiftValue, atCueWithId: nextCue.id, start: true, undoManager: undoManager)
-                                    showPopover = false
-                                }
-                            }
-                        }
+                HStack(spacing: 0)  {
+                    // Shift all remaining
+                    Button {
+                        document.shiftTimeValues(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
+                        showPopover = false
+                    } label: {
+                        Image(systemName: shiftAllSymbol)
+                            .padding([.leading, .trailing], buttonPadding)
                     }
+                        .disabled(Int(shiftValue * 1000) == 0)
+                        .buttonStyle(.borderless)
+                        .help("Shift \(start ? "start" : "end") of current cue and remaining...")
+                    Divider()
+                    
+                    // Shift One
+                    Button {
+                        document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
+                        showPopover = false
+                    } label: {
+                        Image(systemName: shiftOne)
+                            .padding([.leading, .trailing], buttonPadding)
+                    }
+                        .disabled(Int(shiftValue * 1000) == 0)
+                        .buttonStyle(.borderless)
+                        .help("Shift \(start ? "start" : "end") of current cue only")
+                    Divider()
+                    
+                    // Shift Both
+                    Button {
+                        document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: nil, undoManager: undoManager)
+                        showPopover = false
+                    } label: {
+                        HStack {
+                            Image(systemName: shiftBoth)
+                                .padding(0)
+                        }
+                        .padding([.leading, .trailing], buttonPadding)
+                    }
+                        .disabled(Int(shiftValue * 1000) == 0)
+                        .buttonStyle(.borderless)
+                        .help("Shift both start and end of current cue")
+                    if !start {
+                        Divider()
+                        // Shift end and start of next
+                        Button {
+                            document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: false, undoManager: undoManager)
+                            let cueIndex = document.captions.getIndex(forCueID: cue.id)
+                            let nextIndex = cueIndex + 1
+                            let nextCue = document.captions.cues[nextIndex]
+                            document.shiftTime(withValue: shiftValue, atCueWithId: nextCue.id, start: true, undoManager: undoManager)
+                            showPopover = false
+                        } label: {
+                            Image(systemName: endAndStart)
+                                .padding(0)
+                                .rotation3DEffect(.degrees(180), axis: (x: 1, y: 0, z: 0))
+                                .padding([.leading, .trailing], buttonPadding)
+                        }
+                        .disabled(Int(shiftValue * 1000) == 0)
+                        .buttonStyle(.borderless)
+                        .help("Shift end of current cue and start of next")
+                    }
+                }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(Color.secondary.opacity(0.5), lineWidth: 1)
+                )
+
+//                    .contextMenu {
+//                        Section {
+//                            Text("Shift \(isPositive ? "forward" : "backward")")
+//                            Button("\(start ? "start" : "end") and remaining…") {
+//                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
+//                                showPopover = false
+//                            }
+//                        }
+//                        
+//                        Section {
+//                            Button("\(start ? "start" : "end") only") {
+//                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: start, undoManager: undoManager)
+//                                showPopover = false
+//                            }
+//                            Button("both") {
+//                                document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: nil, undoManager: undoManager)
+//                                showPopover = false
+//                            }
+//                            if !start {
+//                                Button("end and start of next") {
+//                                    document.shiftTime(withValue: shiftValue, atCueWithId: cue.id, start: false, undoManager: undoManager)
+//                                    let cueIndex = document.captions.getIndex(forCueID: cue.id)
+//                                    let nextIndex = cueIndex + 1
+//                                    let nextCue = document.captions.cues[nextIndex]
+//                                    document.shiftTime(withValue: shiftValue, atCueWithId: nextCue.id, start: true, undoManager: undoManager)
+//                                    showPopover = false
+//                                }
+//                            }
+//                        }
+//                    }
             }
         }
             .padding()
